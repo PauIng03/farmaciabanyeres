56cb6d550c83b2e1c537a97c8cb58b6129dc8400
PauIng03 <pauessi03@gmail.com>
2025-05-26 13:37:58 +0200

    Index

--- src/App.js
+++ src/App.js
@@ -1,21 +1,10 @@
 import React from 'react';
-import Header from './Header.js';
-import QuiSom from './QuiSom.js';
-import Serveis from './Serveis.js'
-import Blog from './Blog.js'
-import Promocions from './Promocions.js'
-import Footer from './Footer.js'
+import Index from './Index.js';
 
 function App() {
   return (
-    <div className="inici-desktop-1">
-      <Header />
-      <img src="imatges/carrussel-banner-27.png" className="carrussel-banner-27" alt="carrussel-banner" />
-      <QuiSom />
-      <Serveis />
-      <Blog />
-      <Promocions />
-      <Footer />
+    <div>
+      <Index/>
     </div>
   );
 }
