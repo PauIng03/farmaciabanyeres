6b3c9f76434e71de28428397102b589da68d1386
PauIng03 <pauessi03@gmail.com>
2025-05-26 13:42:16 +0200

    Revert "Index"
    This reverts commit 56cb6d550c83b2e1c537a97c8cb58b6129dc8400.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import Index from './Index.js';
+import Header from './Header.js';
+import QuiSom from './QuiSom.js';
+import Serveis from './Serveis.js'
+import Blog from './Blog.js'
+import Promocions from './Promocions.js'
+import Footer from './Footer.js'
 
 function App() {
   return (
-    <div>
-      <Index/>
+    <div className="inici-desktop-1">
+      <Header />
+      <img src="imatges/carrussel-banner-27.png" className="carrussel-banner-27" alt="carrussel-banner" />
+      <QuiSom />
+      <Serveis />
+      <Blog />
+      <Promocions />
+      <Footer />
     </div>
   );
 }
